0ecf1b00b5cae80d9588df1fc44db5b725f063de
Jethro Elano <elanojet@gmail.com>
2026-08-11 20:04:02 +0800

    Week Day 2 python relearn

--- python-learning/week1/day2.py
+++ python-learning/week1/day2.py
@@ -1,37 +1,95 @@
-# Lists
-characters = ["Keqing", "Neuvillette", "Nahida", "Columbina"]
-print(characters[0])    # Access "Keqing" from the chracters list
-print(characters[0:4])  # print the values stored in the list [starting index:stopping index (not included)]
-characters.append("Ineffa") # Adding a value at the end
-characters.append("Furina")
-characters.remove("Ineffa") # Remove by value
-print(characters)
+# Data Structures, f-strings, List Comprehensions
 
-# Observations:
-# The list is dynamic and resizable
+# Four core containers for holding values
+# 1. List - ordered collection of values
+list = ["this", "is", "a", "list"]
+print(list)
 
-# Dict
-friends_ar = {"Knx": 35, "DejaVu": 60, "bread": 60}
-print(friends_ar["bread"])
-print(friends_ar.get("Yotsuha"))    # Use .get to avoid crashing the program in accessing values
-print(friends_ar.get("Yotsuha", 56))   
-print(friends_ar)
+# 2. Dict - a collection of keys and their corresponding value
+dict = {"d":0, "i":1, "c":2, "t":3}
+print(dict)
 
-# Tuple - unchangeable list
-ages = (19.5, 20.7)
-# ages[0] = 20.0 - Will result in an error
+# Tuple - ordered set that can't be changed
+tuple = (1, 2, 3, 4, 5)
+print(tuple)
 
-# Sets
-numbers = {1, 2, 3, 4, 5, 5}
-print(numbers)  # Only shows one of the value with duplicate
+# Set - unordered collection with no duplicates
+set = {"s", "e", "e", "t"}
+print(set)
 
-# Party Roster Tracker
-roster = {"Neuvillette": "DPS", "Sucrose": "Support", "Columbina": "Sub-DPS"}
-roster["Ineffa"] = "Sub-DPS"    # Add key and value to the roster dict
-roster.pop("Sucrose")   # Safely remove Sucrose from the roster
-sub_dps = ["Columbina", "Ineffa"]
-tuple_sub_dps = tuple(sub_dps)
-elements_used = {"hydro", "anemo", "hydro", "electro"}
-print(elements_used)    # Will not print hydro two times
-roster["Ineffa"] = "Shielder"   # Overwrites the value
-print(roster)
+# f-strings - let you insert a variable directly into a sentence
+name = "Jethro"
+print(f"Hello, my name is {name}")
+
+# List Comprehension - shortcut for building lists
+list_copy = [value for value in list]
+print(list_copy)
+
+# Mini Challenges
+list = [1, 2, 2, 3, 4]
+tuple = (1, 2, 2, 3, 4)
+set = {1, 2, 2, 3, 4}
+print(list)
+print(tuple)
+print(set)  # prints the set with no duplicate 2
+
+dict = {"apple": 25, "banana": 15, "orange": 20}
+print(dict.get("apple"))
+dict["grape"] = 1
+print(dict) 
+
+a = 2
+b = 3
+print(f"Total: {a + b}")
+
+# List Comprehension Drills
+squares = []
+for number in range(1, 11):
+    squares.append(number * number)
+print(squares)
+
+squares = [number * number for number in range(1, 11)]
+print(squares)
+
+even_numbers = []
+for number in range(1, 21):
+    if (number % 2 == 0):
+        even_numbers.append(number)
+print(even_numbers)
+
+even_numbers = [number for number in range(1, 21) if number % 2 == 0]
+print(even_numbers)
+
+list = ["lower", "case"]
+uppercase = []
+for word in list:
+    uppercase.append(word.upper())
+print(uppercase)
+
+uppercase = [word.upper() for word in list]
+print(uppercase)
+
+length = []
+for word in list:
+    length.append(len(word))
+print(length)
+
+length = [len(word) for word in list]
+print(length)
+
+first_letter = []
+for word in list:
+    first_letter.append(word[0])
+print(first_letter)
+
+first_letter = [word[0] for word in list]
+print(first_letter)
+
+# Checkpoint Challenge
+question = ["Should", "I", "pull", "for", "Odette", "or", "wait", "for", "Furina?"]
+
+length_uppercase = [word.upper() for word in question if len(word) > 4]
+print(length_uppercase)
+
+length = {word: len(word) for word in question}
+print(length)
