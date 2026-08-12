0932fd83d2f2401d6c5155a260b2b26b2b523647
Jethro Elano <elanojet@gmail.com>
2026-08-12 11:19:46 +0800

    Week 1 day 3 python relearn

--- python-learning/week1/day3.py
+++ python-learning/week1/day3.py
@@ -1,107 +1,74 @@
-# for loop and range()
+# For Loops, While Loops, range()
 
-# for loop attempt
-# for(i = 0; i < 5; i++):
-#    print(i)
+# For Loop - traverses through the items in something like lists, dicts, and etc.
+"""
+Syntax:
+for [item] in [lists, range, etc.]
+"""
+names = ["Ace", "Rover", "Cydrex"]
+for name in names:
+    print(name)
 
-# for loop syntax
-for i in range(5): 
+# range() - when you need to loop through a specific number range
+"""
+Syntax: 
+1. range(end (0 is the start and the end is not included))
+2. range(start, end (not included))
+3. range(start, end (not included), update statement)
+"""
+for number in range(1):
+    print(number)
+
+# While Loop - loops if the condition is met
+"""
+Syntax: 
+while (condition):
+    indented code block/sentence
+"""
+i = 0
+while (i < 5):
     print(i)
-print()
+    i += 1  # update statement
 
-# Question: How about decrement?
-
-for i in range(2, 10):
+# Mini Challenges
+# range()
+for i in range(0, 10):
     print(i)
-print()
-
-# Observation: range(start, end (not included))
-
-for i in range(0, 10, 2):
+for i in range(10, 0, -1):
     print(i)
-print()
 
-# Observation: range(start, end (not included), update statement)
+# For Loop
+list = ["this", "is", "a", "list"]
+for word in list:
+    print(word)
 
-# Attempt for answering my question about decrement
-for i in range(0, -12, -2):
+# While Loop
+i = 5
+while (i > 0):
     print(i)
-print()
+    i -= 1
 
-# looping directly over a list
-chess_pieces = ["king", "queen", "bishop", "knight", "rook", "pawn"]
+# Checkpoint Challenge
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for pieces in chess_pieces:
-    print(pieces)
-print()
+even_numbers = []
+for number in numbers:
+    if (number % 2 == 0):
+        even_numbers.append(number)
+print(even_numbers)
 
-# Question: How to print statements in a single line?
-# Answer
-
-for pieces in chess_pieces:
-    print(pieces, end = " ")
-print()
-
-# while loops
-count = 0
-while (count < 5):
-    print(count)
-    count += 1
-print()
-
-# Observation: ++/-- doesn't exist in python
-
-# f strings
-name = "Jethro"
-age = 19
-print(f"My name is {name} and I am {age}")
-print(f"Next year, I'll be {age + 1}")
-
-# Java Comparison: the f is inside the parenthesis and the variables are inserted directly via braces enclosure
-
-# loop first approach
-squares = []
-for i in range(5):
-    squares.append(i ** 2)
-print(squares)
-# Observation: the ** symbol is exponentation
-
-# list comprehension - one-liner
-# parts [(what gets stored) for (variable) in (what you are looping over)]
-evens = [i for i in range(2, 10, 2)]
-print(evens)
-
-upper_pieces = [pieces.upper() for pieces in chess_pieces]
-print(upper_pieces)
-
-# parts of comprehension list with if
-# [what is being stored for variable in what you are looping over if condition]
-long_pieces = [piece for piece in chess_pieces if len(piece) > 5]
-print(long_pieces)
-
-# challenge
-egg_counts = [2, 0, 1, 3, 2, 0, 1] 
-
-# print the number of eggs per day using a for loop with range
-for i in range(7):
-    print(f"Day  {i + 1} :  {egg_counts[i]} eggs")
-
-# create a list of good days (eggs >= 2) using list comprehension
-good_days = [count for count in egg_counts if count >= 2]
-print(good_days)
-
-# convert egg_counts into f-string labels using a comprehension list
-day_labels = [f"{count} eggs" for count in egg_counts]
-print(day_labels)
-
-# using a while loop, count the eggs until it reaches 5 and print which day it stopped
-count = 0
+sum = 0
+counter = 0
 index = 0
-while(count < 5 and index < len(egg_counts)):
-    count += egg_counts[index]
+while (sum <= 50):
+    sum += even_numbers[index]
     index += 1
-print(f"Day {index}")
+    if (index == len(even_numbers)): 
+        index = 0
+    counter += 1
+print(sum, counter)
 
-# convert egg_counts into f-string labels but only the good days using a comprehension list
-day_labels = [f"{count} eggs" for count in egg_counts if count >= 2]
-print(day_labels)
+even_squares = {}
+for number in even_numbers:
+    even_squares[number] = number * number
+print(even_squares)
